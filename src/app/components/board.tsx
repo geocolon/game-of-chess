6643d92a0f9c15fa69a69d7c5326e55d3bb769de
George Colon <geroge_colon@oneilluminate.com>
2024-03-29 21:55:19 -0700

    board.tsx updated

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -1,7 +1,7 @@
 "use client";
-import {Chess} from 'chess.js';
+import {Chess, Move} from 'chess.js';
 import {useEffect, useState} from 'react';
-import {p0, pw, pb} from '../utils/chess-utils';
+import {p0, pw, pb, chess, getBoard} from '../utils/chess-utils';
 import styles from './board.module.scss';
 
 const Board = () => {
@@ -12,30 +12,51 @@ const Board = () => {
             new Array(8)
             .fill('')
         ));
-
+    const [highlighted, setHighlighted] = useState<string[]>([]);
     useEffect(() => {
-        const chess = new Chess();
-        const b = chess
-            .ascii()
-            .split('\n')
-            .slice(1, 9)
-            .map((rank) => rank.slice(5, 27).split('  '));
-        setPieces(b);
+        setPieces(getBoard());
     }, []);
 
   return (
     <div className={styles.board}>
         {new Array(8).fill(0).map((_, i) => (
             <div className={styles.row} key={i}>
-                {new Array(8).fill(0).map((_, j) => (
-                    <div 
-                        className={[
-                            styles.col, 
-                            (i + j) % 2 === 0 ? styles.w : styles.b 
-                            ].join(' ')} 
-                            key={`${i}, ${j}`}
-                    >{pieces[i][j]}</div>
-                ))}
+                {new Array(8).fill(0).map((_, j) => {
+                        // Which piece is at this square?
+                        let p = pieces[i][j];
+                        let c = '';
+                        if ( p == ".") {
+                            p = "";
+                        } else if (p.match(/[A-Z]/)) {
+                            p = pw[p0.indexOf(p.toLowerCase())];
+                            c = 'w';
+                        } else {
+                            p = pb[p0.indexOf(p)];
+                            c = 'b';
+                        }
+                        
+                        const square = `${'abcdefgh'.charAt(j)}${8 - i}`;
+
+                        return (
+                        <div 
+                            className={[
+                                styles.col, 
+                                (i + j) % 2 === 0 ? styles.w : styles.b,
+                                    p && chess.turn() == c && styles.pointer,
+                                    highlighted.includes(square) && styles.highlighted,
+                                ].join(' ')} 
+                                key={`${i}, ${j}`}
+                                onClick={
+                                   () => {
+                                    // @ts-ignore
+                                    const mvs = chess.moves({square, verbose: true}) as Move[];
+                                    setHighlighted(mvs.map(({to}) => to));
+                                    console.log(mvs);
+                                
+                                }}
+                        >{p}</div>)
+                    }
+                )}
             </div>
             ))}
     </div>
