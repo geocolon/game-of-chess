dc92249665e0dca0f6e5b84e9569000dfa14a095
George Colon <geroge_colon@oneilluminate.com>
2024-03-30 04:16:40 -0700

    save working board, Rooks and Bishops, King queen not working

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {Chess, Move} from 'chess.js';
+import {Move} from 'chess.js';
 import {useEffect, useState} from 'react';
 import {p0, pw, pb, chess, getBoard} from '../utils/chess-utils';
 import styles from './board.module.scss';
@@ -16,7 +16,7 @@ const Board = () => {
     useEffect(() => {
         setPieces(getBoard());
     }, []);
-
+    console.log(highlighted);
   return (
     <div className={styles.board}>
         {new Array(8).fill(0).map((_, i) => (
@@ -40,16 +40,15 @@ const Board = () => {
                             className={[
                                 styles.col, 
                                 (i + j) % 2 === 0 ? styles.w : styles.b,
-                                    p && chess.turn() == c && styles.pointer,
-                                    highlighted.includes(square) && styles.highlighted,
+                                p && chess.turn() == c && styles.pointer,
+                                highlighted.includes(square) && styles.highlighted,
                                 ].join(' ')} 
                                 key={`${i}, ${j}`}
                                 onClick={
                                    () => {
                                     // @ts-ignore
                                     const mvs = chess.moves({square, verbose: true}) as Move[];
-                                    setHighlighted([...mvs.map(({to}) => to)]);
-                                
+                                    setHighlighted(mvs.map(({to}) => to));
                                 }}
                         >{p}</div>)
                     }
